419b627766b9887a9b11aa15d63b5dfe1b1c64ad
defowler2005 <defowler2005@gmail.com>
2024-02-17 10:47:09 +1030

    ggygy

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace defowler2005_app
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            TabControl tabControl = new TabControl();
+            tabControl.TabPages.Add(new TabPage("Tab 1"));
+            tabControl.TabPages.Add(new TabPage("Tab 2"));
+            tabControl.TabPages.Add(new TabPage("Tab 3"));
+
+            tabControl.TabPages[0].Controls.Add(new Label
+            {
+                Text = "Content for Tab 1",
+                AutoSize = true,
+                Location = new System.Drawing.Point(50, 50)
+            });
+
+            tabControl.TabPages[1].Controls.Add(new Label
+            {
+                Text = "Content for Tab 2",
+                AutoSize = true,
+                Location = new System.Drawing.Point(50, 50)
+            });
+
+            tabControl.TabPages[2].Controls.Add(new Label
+            {
+                Text = "Content for Tab 3",
+                AutoSize = true,
+                Location = new System.Drawing.Point(50, 50)
+            });
+
+            tabControl.Dock = DockStyle.Top;
+
+            Form form = new Form
+            {
+                Text = "defowler005's windows app.",
+                Size = new System.Drawing.Size(300, 200),
+                Icon = new Icon("./favicon.ico"),
+                BackgroundImage = Image.FromFile("./background.png")
+            };
+
+
+            Label helloLabel = new Label
+            {
+                Text = "Hello, World!",
+                AutoSize = true,
+                Location = new System.Drawing.Point(50, 50)
+            };
+
+            form.Controls.Add(tabControl);
+            form.Controls.Add(helloLabel);
+            Application.Run(form);
+        }
+    }
+};
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)] // Exposes the types in this assembly to COM components.
+
+[assembly: Guid("00749d06-743b-4fcd-a3e3-36c5db9af897")] // Specifies the GUID associated with the assembly.
+
+[assembly: AssemblyInformationalVersion("0.0.0.1")] // Specifies the informational version of the assembly.
+
+[assembly: AssemblyCompany("Your Company Name")] // Specifies the company that produced the assembly.
+
+[assembly: AssemblyProduct("defowler-win-app")] // Specifies the name of the product.
+
+[assembly: AssemblyCopyright("Copyright © defowler 2024")] // Specifies the copyright information.
+
+[assembly: AssemblyTrademark("You are free to decompile this application")] // Specifies any trademarks associated with the assembly.
+
+[assembly: AssemblyVersion("1.0.0.0")] // Specifies the version of the assembly being attributed.
+
+[assembly: AssemblyFileVersion("0.0.0.1")] // Specifies the Win32 file version resource.
+
+[assembly: AssemblyTitle("defowler-win-app")] // Specifies the title of the assembly.
+
+[assembly: AssemblyDescription("A test file for defowler2005")] // Specifies the description of the assembly.
+
+[assembly: AssemblyConfiguration("")] // Specifies the build configuration.
+
+[assembly: AssemblyCulture("")] // Specifies the culture supported by the assembly.
+
+[assembly: AssemblyKeyFile("")] // Specifies the file containing the key pair.
+
+[assembly: AssemblyDelaySign(false)] // Specifies whether the assembly is to be delay-signed.
+
+[assembly: NeutralResourcesLanguage("")] // Specifies the culture for the resources in the assembly.
